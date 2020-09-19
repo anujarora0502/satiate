8109cf0696871ebe29127b4ee97d12f25397a4e6
anujarora0502 <anujarora0502@gmail.com>
2020-09-20 01:14:43 +0530

    Updates

---
+++ precache-manifest.fee1e9f59eae1655d93968aac0033b2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df91fd2627df6dfbad09b9f7f8fa7a68",
+    "url": "/satiate/index.html"
+  },
+  {
+    "revision": "9fbd1e795bf881602ad3",
+    "url": "/satiate/static/css/main.aa7cfc7d.chunk.css"
+  },
+  {
+    "revision": "3c362568d7f4cc858bfc",
+    "url": "/satiate/static/js/2.65f9d28e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/satiate/static/js/2.65f9d28e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fbd1e795bf881602ad3",
+    "url": "/satiate/static/js/main.508109c0.chunk.js"
+  },
+  {
+    "revision": "99ae56222b680122508b",
+    "url": "/satiate/static/js/runtime-main.0109de43.js"
+  }
+]);
